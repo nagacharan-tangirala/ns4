b1499d32d26cea8bf7d3d9359939e6a5b43bc176
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-28 12:21:53 +0100

    removed: millicar and used 5g-lena project

--- scratch/datahose/include/Core.h
+++ scratch/datahose/include/Core.h
@@ -13,26 +13,58 @@
 #include "NetSetup.h"
 #include "PositionReader.h"
 #include "TraceMobility.h"
-#include "toml.hpp"
+
+#include <toml.hpp>
 
 #include "ns3/constant-position-mobility-model.h"
 #include "ns3/network-module.h"
+#include "ns3/core-module.h"
+#include "ns3/lte-sl-tft.h"
+#include "ns3/node-container.h"
+#include "ns3/nr-helper.h"
+#include "ns3/nr-point-to-point-epc-helper.h"
+#include "ns3/nr-sl-helper.h"
+#include "ns3/nr-ue-net-device.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/applications-module.h"
+#include "ns3/isotropic-antenna-model.h"
+#include "ns3/nr-module.h"
 
 using namespace ns3;
+
+typedef toml::basic_value<toml::discard_comments, std::map, std::vector> toml_value;
 
 class Core
 {
   private:
     std::string m_configFile;
     toml_value m_config;
+
     std::unique_ptr<NetSetup> m_netSetup;
+    Ipv4Address m_groupCastAddr;
 
     Time m_stopTime;
     Time m_streamTime;
 
-    std::unique_ptr<NodeContainer> m_vehicleNodes;
-    std::unique_ptr<NodeContainer> m_rsuNodes;
-    std::unique_ptr<NodeContainer> m_controllerNodes;
+    uint32_t m_numVehicles;
+    uint32_t m_numRSUs;
+    uint32_t m_numControllers;
+
+    NodeContainer m_vehicleNodes;
+    NodeContainer m_rsuNodes;
+    NodeContainer m_controllerNodes;
+
+    NodeContainer m_allNodes;
+
+    activation_map_t m_vehicleActivationTimes;
+    activation_map_t m_rsuActivationTimes;
+    activation_map_t m_controllerActivationTimes;
+
+    NetDeviceContainer m_vehicleDevices;
+    NetDeviceContainer m_rsuDevices;
+    NetDeviceContainer m_controllerDevices;
+
+    NetDeviceContainer m_allDevices;
 
     std::unique_ptr<TraceMobility> m_vehicleMobility;
 
@@ -62,11 +94,13 @@ class Core
 
     static activation_map_t getActivationData(const toml_value& nodeSettings);
 
+    void run();
+
   public:
     Core(std::string configFile);
 
-    bool build();
+    void runSimulation();
 
-    Time getStopTime();
+    void segregateNetDevices();
 };
 #endif // NS3_CORE_H
