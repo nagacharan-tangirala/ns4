55444b4d83135617df744844be10ae606003aecc
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-28 16:05:11 +0100

    added: output writer class

--- scratch/datahose/include/Outputter.h
+++ scratch/datahose/include/Outputter.h
@@ -1,8 +1,154 @@
-//
-// Created by charan on 28.11.23.
-//
+/*
+ * Outputter.h
+ *
+ * Created on: 2023-11-28
+ * Author: charan
+ */
 
 #ifndef NS3_OUTPUTTER_H
 #define NS3_OUTPUTTER_H
 
+#include "Columns.h"
+#include "ue-mac-pscch-tx-output-stats.h"
+#include "ue-mac-pssch-tx-output-stats.h"
+#include "ue-phy-pscch-rx-output-stats.h"
+#include "ue-phy-pssch-rx-output-stats.h"
+#include "ue-rlc-rx-output-stats.h"
+#include "ue-to-ue-pkt-txrx-output-stats.h"
+#include "v2x-kpi.h"
+
+#include "ns3/config.h"
+#include "ns3/ipv4-l3-protocol.h"
+#include "ns3/mobility-model.h"
+#include "ns3/nr-ue-mac.h"
+#include "ns3/nr-ue-net-device.h"
+#include "ns3/seq-ts-size-header.h"
+#include "ns3/sqlite-output.h"
+
+#include <toml.hpp>
+
+using namespace ns3;
+
+class Outputter
+{
+  private:
+    std::string m_outputPath;
+    std::string m_outputName;
+    Time m_duration;
+    UeMacPscchTxOutputStats m_pscchStats;
+    UeMacPsschTxOutputStats m_psschStats;
+
+    UePhyPsschRxOutputStats m_psschPhyStats;
+    UePhyPscchRxOutputStats m_pscchPhyStats;
+
+    UeRlcRxOutputStats m_ueRlcRxStats;
+    UeToUePktTxRxOutputStats m_pktStats;
+    V2xKpi m_v2xKpi;
+
+  public:
+    Outputter(std::string outputPath, std::string outputName, Time duration);
+
+    void dumpCache();
+
+    void configureDatabase(ApplicationContainer clientApps, ApplicationContainer serverApps);
+
+    /**
+     * \brief Method to listen the trace SlPscchScheduling of NrUeMac, which gets
+     *        triggered upon the transmission of SCI format 1-A from UE MAC.
+     *
+     * \param pscchStats Pointer to the UeMacPscchTxOutputStats class,
+     *        which is responsible to write the trace source parameters to a database.
+     * \param pscchStatsParams Parameters of the trace source.
+     */
+    static void NotifySlPscchScheduling(UeMacPscchTxOutputStats* pscchStats,
+                                        const SlPscchUeMacStatParameters pscchStatsParams);
+
+    /**
+     * \brief Method to listen the trace SlPsschScheduling of NrUeMac, which gets
+     *        triggered upon the transmission of SCI format 2-A and data from UE MAC.
+     *
+     * \param psschStats Pointer to the UeMacPsschTxOutputStats class,
+     *        which is responsible to write the trace source parameters to a database.
+     * \param psschStatsParams Parameters of the trace source.
+     */
+    static void NotifySlPsschScheduling(UeMacPsschTxOutputStats* psschStats,
+                                        const SlPsschUeMacStatParameters psschStatsParams);
+
+    /**
+     * \brief Method to listen the trace RxPscchTraceUe of NrSpectrumPhy, which gets
+     *        triggered upon the reception of SCI format 1-A.
+     *
+     * \param pscchStats Pointer to the UePhyPscchRxOutputStats class,
+     *        which is responsible to write the trace source parameters to a database.
+     * \param pscchStatsParams Parameters of the trace source.
+     */
+    static void NotifySlPscchRx(UePhyPscchRxOutputStats* pscchStats,
+                                const SlRxCtrlPacketTraceParams pscchStatsParams);
+
+    /**
+     * \brief Method to listen the trace RxPsschTraceUe of NrSpectrumPhy, which gets
+     *        triggered upon the reception of SCI format 2-A and data.
+     *
+     * \param psschStats Pointer to the UePhyPsschRxOutputStats class,
+     *        which is responsible to write the trace source parameters to a database.
+     * \param psschStatsParams Parameters of the trace source.
+     */
+    static void NotifySlPsschRx(UePhyPsschRxOutputStats* psschStats,
+                                const SlRxDataPacketTraceParams psschStatsParams);
+
+    /**
+     * \brief Method to listen the application level traces of type TxWithAddresses
+     *        and RxWithAddresses.
+     * \param stats Pointer to the UeToUePktTxRxOutputStats class,
+     *        which is responsible to write the trace source parameters to a database.
+     * \param node The pointer to the TX or RX node
+     * \param localAddrs The local IPV4 address of the node
+     * \param txRx The string indicating the type of node, i.e., TX or RX
+     * \param p The packet
+     * \param srcAddrs The source address from the trace
+     * \param dstAddrs The destination address from the trace
+     * \param seqTsSizeHeader The SeqTsSizeHeader
+     */
+    static void UePacketTraceDb(UeToUePktTxRxOutputStats* stats,
+                                Ptr<Node> node,
+                                const Address& localAddrs,
+                                std::string txRx,
+                                Ptr<const Packet> p,
+                                const Address& srcAddrs,
+                                const Address& dstAddrs,
+                                const SeqTsSizeHeader& seqTsSizeHeader);
+
+    /**
+     * \brief Trace sink for RxRlcPduWithTxRnti trace of NrUeMac
+     * \param stats Pointer to UeRlcRxOutputStats API responsible to write the
+     *        information communicated by this trace into a database.
+     * \param imsi The IMSI of the UE
+     * \param rnti The RNTI of the UE
+     * \param txRnti The RNTI of the TX UE
+     * \param lcid The logical channel id
+     * \param rxPduSize The received PDU size
+     * \param delay The end-to-end, i.e., from TX RLC entity to RX
+     *        RLC entity, delay in Seconds.
+     */
+    static void NotifySlRlcPduRx(UeRlcRxOutputStats* stats,
+                                 uint64_t imsi,
+                                 uint16_t rnti,
+                                 uint16_t txRnti,
+                                 uint8_t lcid,
+                                 uint32_t rxPduSize,
+                                 double delay);
+
+    /**
+     * \brief Save position of the UE as per its IP address
+     * \param v2xKpi pointer to the V2xKpi API storing the IP of an UE and its position.
+     */
+    static void SavePositionPerIP(V2xKpi* v2xKpi);
+
+    /**
+     * \brief Record mobility of the vehicle UEs every second
+     * \param FirstWrite If this flag is true, write from scratch, otherwise, append to the file
+     * \param fileName Name of the file in which to write the positions
+     */
+    static void RecordMobility(bool FirstWrite, std::string fileName);
+};
 #endif // NS3_OUTPUTTER_H
--- scratch/datahose/src/Outputter.cpp
+++ scratch/datahose/src/Outputter.cpp
@@ -1,3 +1,220 @@
-//
-// Created by charan on 28.11.23.
-//
+#include "Outputter.h"
+
+NS_LOG_COMPONENT_DEFINE("Outputter");
+
+Outputter::Outputter(std::string outputPath, std::string outputName, Time simDuration)
+{
+    this->m_outputPath = outputPath;
+    this->m_outputName = outputName;
+    this->m_duration = simDuration;
+}
+
+void
+Outputter::configureDatabase(ApplicationContainer clientApps, ApplicationContainer serverApps)
+{
+    std::string dbPath = m_outputPath +  m_outputName + ".db";
+    NS_LOG_DEBUG("Outputter: Database path: " << dbPath);
+    SQLiteOutput db(dbPath);
+
+    NS_LOG_DEBUG("Outputter: Connecting to SlPscchScheduling trace source");
+    UeMacPscchTxOutputStats pscchStats;
+    pscchStats.SetDb(&db, "pscchTxUeMac");
+    Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::NrUeNetDevice/"
+                                  "ComponentCarrierMapUe/*/NrUeMac/SlPscchScheduling",
+                                  MakeBoundCallback(&NotifySlPscchScheduling, &this->m_pscchStats));
+
+    NS_LOG_DEBUG("Outputter: Connecting to SlPsschScheduling trace source");
+    this->m_psschStats.SetDb(&db, "psschTxUeMac");
+    Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::NrUeNetDevice/"
+                                  "ComponentCarrierMapUe/*/NrUeMac/SlPsschScheduling",
+                                  MakeBoundCallback(&NotifySlPsschScheduling, &this->m_psschStats));
+
+    NS_LOG_DEBUG("Outputter: Connecting to RxPscchTraceUe trace source");
+    this->m_pscchPhyStats.SetDb(&db, "pscchRxUePhy");
+    Config::ConnectWithoutContext(
+        "/NodeList/*/DeviceList/*/$ns3::NrUeNetDevice/ComponentCarrierMapUe/*/NrUePhy/"
+        "NrSpectrumPhyList/*/RxPscchTraceUe",
+        MakeBoundCallback(&NotifySlPscchRx, &this->m_pscchPhyStats));
+
+    NS_LOG_DEBUG("Outputter: Connecting to RxPsschTraceUe trace source");
+    this->m_psschPhyStats.SetDb(&db, "psschRxUePhy");
+    Config::ConnectWithoutContext(
+        "/NodeList/*/DeviceList/*/$ns3::NrUeNetDevice/ComponentCarrierMapUe/*/NrUePhy/"
+        "NrSpectrumPhyList/*/RxPsschTraceUe",
+        MakeBoundCallback(&NotifySlPsschRx, &this->m_psschPhyStats));
+
+    NS_LOG_DEBUG("Outputter: Connecting to RxRlcPduWithTxRnti trace source");
+    this->m_ueRlcRxStats.SetDb(&db, "rlcRx");
+    Config::ConnectWithoutContext("/NodeList/*/DeviceList/*/$ns3::NrUeNetDevice/"
+                                  "ComponentCarrierMapUe/*/NrUeMac/RxRlcPduWithTxRnti",
+                                  MakeBoundCallback(&NotifySlRlcPduRx, &this->m_ueRlcRxStats));
+
+    this->m_pktStats.SetDb(&db, "pktTxRx");
+
+    for (uint32_t ac = 0; ac < clientApps.GetN(); ac++)
+    {
+        Ipv4Address localAddrs =
+            clientApps.Get(ac)->GetNode()->GetObject<Ipv4L3Protocol>()->GetAddress(1, 0).GetLocal();
+        NS_LOG_DEBUG("Setting Tx trace for address: " << localAddrs);
+        clientApps.Get(ac)->TraceConnect("TxWithSeqTsSize",
+                                         "tx",
+                                         MakeBoundCallback(&UePacketTraceDb,
+                                                           &this->m_pktStats,
+                                                           clientApps.Get(ac)->GetNode(),
+                                                           localAddrs));
+    }
+
+    // Set Rx traces
+    for (uint32_t ac = 0; ac < serverApps.GetN(); ac++)
+    {
+        Ipv4Address localAddrs =
+            serverApps.Get(ac)->GetNode()->GetObject<Ipv4L3Protocol>()->GetAddress(1, 0).GetLocal();
+        NS_LOG_DEBUG("Setting Rx trace for address: " << localAddrs);
+        serverApps.Get(ac)->TraceConnect("RxWithSeqTsSize",
+                                         "rx",
+                                         MakeBoundCallback(&UePacketTraceDb,
+                                                           &this->m_pktStats,
+                                                           serverApps.Get(ac)->GetNode(),
+                                                           localAddrs));
+    }
+
+    NS_LOG_DEBUG("Outputter: Connecting to V2xKpi trace source");
+    std::string v2xkpiPath = this->m_outputPath + this->m_outputName + "_v2xkpi";
+    this->m_v2xKpi.SetDbPath(v2xkpiPath);
+    this->m_v2xKpi.SetTxAppDuration(this->m_duration.GetSeconds());
+    SavePositionPerIP(&this->m_v2xKpi);
+    this->m_v2xKpi.SetRangeForV2xKpis(200);
+
+    std::string mobilityFileName = this->m_outputPath + "mobility-ues.txt";
+    NS_LOG_DEBUG("Outputter: Connecting to Mobility trace source");
+    RecordMobility(true, mobilityFileName);
+}
+
+void
+    Outputter::dumpCache()
+{
+    this->m_pscchStats.EmptyCache();
+    this->m_psschStats.EmptyCache();
+    this->m_pscchPhyStats.EmptyCache();
+    this->m_psschPhyStats.EmptyCache();
+    this->m_ueRlcRxStats.EmptyCache();
+    this->m_v2xKpi.WriteKpis();
+    this->m_pktStats.EmptyCache();
+}
+
+void
+Outputter::NotifySlPscchScheduling(UeMacPscchTxOutputStats* pscchStats,
+                                   const SlPscchUeMacStatParameters pscchStatsParams)
+{
+    pscchStats->Save(pscchStatsParams);
+}
+
+void
+Outputter::NotifySlPsschScheduling(UeMacPsschTxOutputStats* psschStats,
+                                   const SlPsschUeMacStatParameters psschStatsParams)
+{
+    psschStats->Save(psschStatsParams);
+}
+
+void
+Outputter::NotifySlPscchRx(UePhyPscchRxOutputStats* pscchStats,
+                           const SlRxCtrlPacketTraceParams pscchStatsParams)
+{
+    pscchStats->Save(pscchStatsParams);
+}
+
+void
+Outputter::NotifySlPsschRx(UePhyPsschRxOutputStats* psschStats,
+                           const SlRxDataPacketTraceParams psschStatsParams)
+{
+    psschStats->Save(psschStatsParams);
+}
+
+void
+Outputter::UePacketTraceDb(UeToUePktTxRxOutputStats* stats,
+                           Ptr<Node> node,
+                           const Address& localAddrs,
+                           std::string txRx,
+                           Ptr<const Packet> p,
+                           const Address& srcAddrs,
+                           const Address& dstAddrs,
+                           const SeqTsSizeHeader& seqTsSizeHeader)
+{
+    uint32_t nodeId = node->GetId();
+    uint64_t imsi = node->GetDevice(0)->GetObject<NrUeNetDevice>()->GetImsi();
+    uint32_t seq = seqTsSizeHeader.GetSeq();
+    uint32_t pktSize = p->GetSize() + seqTsSizeHeader.GetSerializedSize();
+
+    stats->Save(txRx, localAddrs, nodeId, imsi, pktSize, srcAddrs, dstAddrs, seq);
+}
+
+void
+Outputter::NotifySlRlcPduRx(UeRlcRxOutputStats* stats,
+                            uint64_t imsi,
+                            uint16_t rnti,
+                            uint16_t txRnti,
+                            uint8_t lcid,
+                            uint32_t rxPduSize,
+                            double delay)
+{
+    stats->Save(imsi, rnti, txRnti, lcid, rxPduSize, delay);
+}
+
+void
+Outputter::SavePositionPerIP(V2xKpi* v2xKpi)
+{
+    for (auto it = NodeList::Begin(); it != NodeList::End(); ++it)
+    {
+        Ptr<Node> node = *it;
+        int nDevs = node->GetNDevices();
+        for (int j = 0; j < nDevs; j++)
+        {
+            Ptr<NrUeNetDevice> uedev = node->GetDevice(j)->GetObject<NrUeNetDevice>();
+            if (uedev)
+            {
+                Ptr<Ipv4L3Protocol> ipv4Protocol = node->GetObject<Ipv4L3Protocol>();
+                Ipv4InterfaceAddress addresses = ipv4Protocol->GetAddress(1, 0);
+                std::ostringstream ueIpv4Addr;
+                ueIpv4Addr.str("");
+                ueIpv4Addr << addresses.GetLocal();
+                Vector pos = node->GetObject<MobilityModel>()->GetPosition();
+                v2xKpi->FillPosPerIpMap(ueIpv4Addr.str(), pos);
+            }
+        }
+    }
+}
+
+void
+Outputter::RecordMobility(bool FirstWrite, std::string fileName)
+{
+    std::ofstream outFile;
+    if (FirstWrite)
+    {
+        outFile.open(fileName.c_str(), std::ios_base::out);
+        FirstWrite = false;
+    }
+    else
+    {
+        outFile.open(fileName.c_str(), std::ios_base::app);
+        outFile << std::endl;
+        outFile << std::endl;
+    }
+
+    for (auto it = NodeList::Begin(); it != NodeList::End(); ++it)
+    {
+        Ptr<Node> node = *it;
+        int nDevs = node->GetNDevices();
+        for (int j = 0; j < nDevs; j++)
+        {
+            Ptr<NrUeNetDevice> uedev = node->GetDevice(j)->GetObject<NrUeNetDevice>();
+            if (uedev)
+            {
+                Vector pos = node->GetObject<MobilityModel>()->GetPosition();
+                outFile << Simulator::Now().GetSeconds() << " " << node->GetId() << " "
+                        << uedev->GetImsi() << " " << pos.x << " " << pos.y << std::endl;
+            }
+        }
+    }
+
+    Simulator::Schedule(Seconds(0.1), &Outputter::RecordMobility, FirstWrite, fileName);
+}
