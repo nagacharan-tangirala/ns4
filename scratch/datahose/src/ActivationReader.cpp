1d514cb9e7b3d6453f1f8a34960a5e53bc9bc16d
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-11-29 20:54:23 +0100

    fixed: activation data reading function

--- scratch/datahose/src/ActivationReader.cpp
+++ scratch/datahose/src/ActivationReader.cpp
@@ -26,26 +26,25 @@ ActivationReader::getActivationTimes()
         auto nodeIdVal = nodeIdData->GetScalar(rowIdx).ValueOrDie();
         uint64_t nodeId = std::static_pointer_cast<arrow::Int64Scalar>(nodeIdVal)->value;
 
-        Time onTime = getTimeFromTimeColumn(onTimeCol, rowIdx);
-        Time offTime = getTimeFromTimeColumn(offTimeCol, rowIdx);
+        auto onTimeVal = onTimeCol->GetScalar(rowIdx).ValueOrDie();
+        uint64_t onTime = std::static_pointer_cast<arrow::Int64Scalar>(onTimeVal)->value;
+        Time onTimeMs = Time(MilliSeconds(onTime));
 
-        NS_LOG_DEBUG("Node ID: " << nodeId << " On time: " << onTime.GetMilliSeconds()
-                                 << " Off time: " << offTime.GetMilliSeconds());
+        auto offTimeVal = offTimeCol->GetScalar(rowIdx).ValueOrDie();
+        uint64_t offTime = std::static_pointer_cast<arrow::Int64Scalar>(offTimeVal)->value;
+        Time offTimeMs = Time(MilliSeconds(offTime));
+
+        NS_LOG_DEBUG("Node ID: " << nodeId << " On time: " << onTimeMs.GetMilliSeconds()
+                                 << " Off time: " << offTimeMs.GetMilliSeconds());
+
+        if (activationTimes.find(nodeId) != activationTimes.end())
+        {
+            NS_LOG_UNCOND("Overwriting activation times for node " << nodeId);
+        }
         activationTimes.insert(
             std::make_pair(nodeId, std::make_pair(onTime, offTime)));
     }
     return activationTimes;
-}
-
-Time
-ActivationReader::getTimeFromTimeColumn(const std::shared_ptr<arrow::ChunkedArray>& timeColumn,
-                                        int position) const
-{
-    auto onTimeVal = timeColumn->GetScalar(position).ValueOrDie();
-    auto onTimeList = std::static_pointer_cast<arrow::ListScalar>(onTimeVal)->value;
-    auto onTimeScalar = onTimeList->GetScalar(0).ValueOrDie();
-    uint64_t onTime = std::static_pointer_cast<arrow::Int64Scalar>(onTimeScalar)->value;
-    return MilliSeconds(onTime);
 }
 
 std::shared_ptr<arrow::Table>
