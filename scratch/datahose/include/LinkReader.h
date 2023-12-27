6d1683177810de2a96deae8323849477a1af1423
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-12-27 19:10:12 +0100

    added: first version of link reading

--- scratch/datahose/include/LinkReader.h
+++ scratch/datahose/include/LinkReader.h
@@ -1,8 +1,40 @@
-//
-// Created by charan on 26.12.23.
-//
+/*
+ * LinkReader.h
+ *
+ * Created on: 2023-12-26
+ * Author: charan
+ */
 
 #ifndef NS3_LINKREADER_H
 #define NS3_LINKREADER_H
+
+#include "Columns.h"
+
+#include "ns3/log.h"
+#include "ns3/nstime.h"
+
+#include <arrow/array.h>
+#include <arrow/io/file.h>
+#include <arrow/scalar.h>
+#include <arrow/table.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/file_reader.h>
+
+using namespace ns3;
+
+typedef std::map<uint32_t, std::vector<std::pair<Time, uint32_t>>> link_map_t; // <nodeId, <time, nodeId>>
+
+class LinkReader
+{
+  private:
+    std::shared_ptr<arrow::io::ReadableFile> m_linkFile;
+
+    std::shared_ptr<arrow::Table> readLinkData();
+
+  public:
+    explicit LinkReader(const std::string& filename);
+
+    link_map_t getLinks();
+};
 
 #endif // NS3_LINKREADER_H
