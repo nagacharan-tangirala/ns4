e54feb0ea5ccf73b39f1fa194b0aca25f04d9799
Nagacharan Teja Tangirala <nagacharan.tangirala@tum.de>
2023-12-27 19:08:00 +0100

    added: ability to read links between devices

---
+++ scratch/datahose/include/LinkReader.h
@@ -0,0 +1,8 @@
+//
+// Created by charan on 26.12.23.
+//
+
+#ifndef NS3_LINKREADER_H
+#define NS3_LINKREADER_H
+
+#endif // NS3_LINKREADER_H
---
+++ scratch/datahose/src/LinkReader.cpp
@@ -0,0 +1,3 @@
+//
+// Created by charan on 26.12.23.
+//
